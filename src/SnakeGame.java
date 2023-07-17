fa66efcd55a78aeac5b567cc7fa00bddaf0e2a42
urstrulyrohan <panderohan15@gmail.com>
2023-07-17 18:42:18 +0530

    first commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class SnakeGame extends JFrame
+{
+        Board board;
+        SnakeGame() {
+            board = new Board();
+            add(board);
+            pack();
+            setResizable(false);
+            setVisible(true);
+        }
+
+        //Initialize Snake Game
+        public static void main(String[] args) {
+            SnakeGame snakeGame = new SnakeGame();
+        }
+}
